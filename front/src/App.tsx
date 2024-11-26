f14716dec5437cca8d2c2efcf5f9dee7ba6942d6
SEITCHA Judicael <sjudicael3@gmail.com>
2024-11-26 14:16:35 +0100

    MAJ

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default  function App() {
   return (
     <div className="bg-neonPurple h-screen flex flex-col items-center justify-center">
-        <h1 className="text-neonGreen text-5xl font-bold">Bienvenue sur le site Cyberpunk!</h1>
+        <h1 className="text-neonGreen text-5xl font-bold">Bienvenue sur le site PokéGo</h1>
         <p className="text-neonYellow text-xl mt-4">
-            Explorez nos produits futuristes.
+            Obtenez vos meilleurs pokémons ici !
         </p>
     </div>
   );
