56324718a1d5f103fc8c7703f63b9c9657283f2f
SEITCHA Judicael <sjudicael3@gmail.com>
2024-12-08 18:20:48 +0100

    Last VF

--- back/product/views.py
+++ back/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import status
 
 from .filters import ProductsFilter
 from .serializers import ProductSerializer
@@ -10,14 +11,13 @@ from .models import Product
 @api_view(['GET'])
 def get_products(request):
     filterset = ProductsFilter(request.GET, queryset=Product.objects.all().order_by('id'))
-    resPerPage = 5
+    resPerPage = 10
     paginator = PageNumberPagination()
     paginator.page_size = resPerPage
     queryset = paginator.paginate_queryset(filterset.qs, request)
     serializer = ProductSerializer(queryset, many=True)
 
     return Response(serializer.data)
-
 
 @api_view(['GET'])
 def get_product(request, pk):
@@ -35,13 +35,12 @@ def delete_product(request, pk):
 
 @api_view(['POST'])
 def create_product(request):
-    serializer = ProductSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
 def update_product(request, pk):
